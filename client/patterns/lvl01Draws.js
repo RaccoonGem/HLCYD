18790bfdd0289bcf568e8f8bcad891eca4f247b6
RaccoonGem <jack@honeydog.org>
2023-09-29 09:35:43 -0500

    adjust enemy 1 appearance

--- client/patterns/lvl01Draws.js
+++ client/patterns/lvl01Draws.js
@@ -7,8 +7,12 @@ let draws = [
     ctx.rotate(game.calcAngle(E, P));
     ctx.beginPath();
     ctx.moveTo(E.size / 2, 0);
+    ctx.lineTo(0, E.size / 4);
+    ctx.lineTo(E.size / 8, 3 * E.size / 8);
     ctx.lineTo(E.size / -2, E.size / 2);
     ctx.lineTo(E.size / -2, E.size / -2);
+    ctx.lineTo(E.size / 8, 3 * E.size / -8);
+    ctx.lineTo(0, E.size / -4);
     ctx.lineTo(E.size / 2, 0);
     ctx.fill();
     ctx.resetTransform();
